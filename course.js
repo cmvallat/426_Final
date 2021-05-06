607a75c0eb99d12dbf698b3f10dccd4521a12434
cmvallat <43908900+cmvallat@users.noreply.github.com>
2021-05-06 09:04:20 -0400

    hello

--- course.js
+++ course.js
@@ -1,18 +1,8 @@
 const { get } = require("browser-sync")
+const cors = require('cors');
 
 $(document).on('ready', function()
 {
-    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-        DeviceOrientationEvent.requestPermission()
-          .then(permissionState => {
-            if (permissionState === 'granted') {
-              window.addEventListener('deviceorientation', () => {});
-            }
-          })
-          .catch(console.error);
-      } else {
-        // handle regular non iOS 13+ devices
-      }
     const URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyAVa4Y29aYOjbRE6VUPaiWrxcGTjjANAZM&location=40.5863028,-75.3552942&radius=50&type=hospital'
     $.ajax({
         url: URL,
