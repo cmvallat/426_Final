09a38783a9da1be53967829b65a972ace87de1ae
cmvallat <43908900+cmvallat@users.noreply.github.com>
2021-05-06 18:11:15 -0400

    test for cookie

--- course.js
+++ course.js
@@ -1,9 +1,9 @@
-const { get } = require("browser-sync")
-const cors = require('cors');
 
 $(document).on('ready', function()
 {
-    const URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyAVa4Y29aYOjbRE6VUPaiWrxcGTjjANAZM&location=40.5863028,-75.3552942&radius=50&type=hospital'
+    console.log("before");
+    /*const URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyAVa4Y29aYOjbRE6VUPaiWrxcGTjjANAZM&location=40.5863028,-75.3552942&radius=50&type=hospital'
+    console.log("before1");
     $.ajax({
         url: URL,
         type: "GET",
@@ -15,4 +15,6 @@ $(document).on('ready', function()
         console.log(`Error ${error}`)
     }
     })
+    console.log("after");
+    */
 })
