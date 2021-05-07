df02c6baa4c253bd782e0312141b6099626a316a
cmvallat <43908900+cmvallat@users.noreply.github.com>
2021-05-07 13:10:08 -0400

    weather api WORKS

--- course.js
+++ course.js
@@ -1,25 +1,33 @@
 
-//console.log(window.top);
 $(function()
 {
-    //console.log("before");
-    // in case of use 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyCGRpqvx7p2BirqPpxHjytmDFFCpAGm6lI&location=40.5863028,-75.3552942&radius=50&type=hospital'
-    const URL = 'https://fcc-weather-api.glitch.me/api/current?lat=53.70&lon=-1.24'
-    //console.log("before1");
-    $.ajax({
+    //CALLING A WEATHER API FROM FREE CODE CAMP - MY ATTEMPT AT 20 API POINTS
+    // in case of fixing issue, use 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyCGRpqvx7p2BirqPpxHjytmDFFCpAGm6lI&location=40.5863028,-75.3552942&radius=50&type=hospital'
+    var lat = 40.5863028;
+    var lon = -75.3552942;
+    const URL = 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + lon
+    $.ajax(
+    {
         url: URL,
         type: "GET",
-        //headers: {'Access-Control-Allow-Origin': '*'},
-        success: function(result){
-        console.log("The temperature during in " + result.name + "will be: " + result.main.temp);
-    },
-    error: function(error)
-    {
-        console.log(`Error ${error}`)
-    }
-    })
-    //console.log("after");
-   
+        success: function(result)
+        {
+            //convert to farenheit cause we love america
+            var CtoF = 9 / 5;
+            var F = ((result.main.temp * CtoF) + 32);
+            var final = Math.floor(F);
 
+            //display weather data
+            $("#formone").html("The temperature will be: " + final + "&#176");
+            $("#formtwo").html("The cloud coverage will be: " + result.weather[0].description);
+            $("#formthree").html("The wind will be: " + result.wind.speed + " mph");
+            console.log(result);
+        },
+
+        error: function(error)
+        {
+            console.log(`Error ${error}`)
+        }
+    })
 });
 
