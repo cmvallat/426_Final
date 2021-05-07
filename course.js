23c0b8bfa09640112519a0db7aad2e5623bd401f
cmvallat <43908900+cmvallat@users.noreply.github.com>
2021-05-07 00:28:16 -0400

    test for heroku

--- course.js
+++ course.js
@@ -1,12 +1,14 @@
 
-$(document).on('ready', function()
+console.log(window.top);
+$(function()
 {
     console.log("before");
-    /*const URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyAVa4Y29aYOjbRE6VUPaiWrxcGTjjANAZM&location=40.5863028,-75.3552942&radius=50&type=hospital'
+    const URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyCGRpqvx7p2BirqPpxHjytmDFFCpAGm6lI&location=40.5863028,-75.3552942&radius=50&type=hospital'
     console.log("before1");
     $.ajax({
         url: URL,
         type: "GET",
+        //headers: {'Access-Control-Allow-Origin': '*'},
         success: function(result){
         console.log(result)
     },
@@ -16,5 +18,5 @@ $(document).on('ready', function()
     }
     })
     console.log("after");
-    */
+    
 })
