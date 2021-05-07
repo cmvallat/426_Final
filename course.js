b379a5d2e027153b6ec5e934e2930472cf770532
cmvallat <43908900+cmvallat@users.noreply.github.com>
2021-05-07 02:56:09 -0400

    testing for temperature

--- course.js
+++ course.js
@@ -1,22 +1,25 @@
 
-console.log(window.top);
+//console.log(window.top);
 $(function()
 {
-    console.log("before");
-    const URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyCGRpqvx7p2BirqPpxHjytmDFFCpAGm6lI&location=40.5863028,-75.3552942&radius=50&type=hospital'
-    console.log("before1");
+    //console.log("before");
+    // in case of use 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyCGRpqvx7p2BirqPpxHjytmDFFCpAGm6lI&location=40.5863028,-75.3552942&radius=50&type=hospital'
+    const URL = 'https://fcc-weather-api.glitch.me/api/current?lat=53.70&lon=-1.24'
+    //console.log("before1");
     $.ajax({
         url: URL,
         type: "GET",
         //headers: {'Access-Control-Allow-Origin': '*'},
         success: function(result){
-        console.log(result)
+        console.log("The temperature during today's race will be: " + result.name);
     },
     error: function(error)
     {
         console.log(`Error ${error}`)
     }
     })
-    console.log("after");
-    
-})
+    //console.log("after");
+   
+
+});
+
