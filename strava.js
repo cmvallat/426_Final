44c18daf9a3c3989ba8bf48be59ebdc52d7fbca8
cmvallat <43908900+cmvallat@users.noreply.github.com>
2021-05-08 01:19:21 -0400

    second attempt, css updates and both apis working

---
+++ strava.js
@@ -0,0 +1,8 @@
+function getActivities()
+{
+  const activities_link = "https://www.strava.com/api/v3/athlete/activities?access_token=ab853b9881304f0290cdbe25c40e31cd0fa1229b"
+  fetch(activities_link)
+    .then((res) => console.log(res.json));
+}
+
+getActivities();
