3a00864bfc9ecf1fe6773a0daa5c018fbb350386
cmvallat <43908900+cmvallat@users.noreply.github.com>
2021-05-08 15:14:58 -0400

    final deploy

---
+++ userlogin.js
@@ -0,0 +1,48 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAogoNhW6Xviae9I5KweXYgnKa8Ng0WrO8",
+    authDomain: "xcsimulator.firebaseapp.com",
+    databaseURL: "https://xcsimulator-default-rtdb.firebaseio.com",
+    projectId: "xcsimulator",
+    storageBucket: "xcsimulator.appspot.com",
+    messagingSenderId: "1043486474965",
+    appId: "1:1043486474965:web:132d74a95e1192bad4985c",
+    measurementId: "G-TSC41N7LVX"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const txtEmail = document.getElementById('txtEmail');
+const txtPassword = document.getElementById('txtPassword');
+const btnSignUp = document.getElementById('btnSignUp');
+const btnLogin = document.getElementById('btnLogin');
+
+btnLogin.addEventListener('click', e =>
+{
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+});
+
+btnSignUp.addEventListener('click', e =>
+{
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+});
+
+firebase.auth().onAuthStateChanged(firebaseUser =>
+    {
+        if(firebaseUser)
+        {
+            console.log(firebaseUser)
+        }
+        else
+        {
+            console.log("not logged in");
+        }
+    });
