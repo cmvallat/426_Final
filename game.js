a76ca7abf5193f9f174627c3da4fe5b2cac5a59a
cmvallat <43908900+cmvallat@users.noreply.github.com>
2021-05-17 20:21:21 -0400

    complete game functionality

--- game.js
+++ game.js
@@ -2,6 +2,7 @@
 
 //start timer on load
 var enterDate = new Date();
+var started = false;
 
 function secondsSinceEnter() //get total time
 {
@@ -10,9 +11,10 @@ function secondsSinceEnter() //get total time
 
 
 
-document.getElementById('start').addEventListener('click',function() 
+document.getElementById('red').addEventListener('click',function() 
 {
     enterDate = new Date();
+    started = true;
     console.log("button clicked");
     document.getElementById("hill").style.animationPlayState = "running";
     document.getElementById("opp").style.animationPlayState = "running";
@@ -24,45 +26,73 @@ document.getElementById('start').addEventListener('click',function()
 //when user runs into an obstacle, get final time
 
 //if user wins
-document.getElementById("finish").onmouseover = function() {
-    var sec = secondsSinceEnter();
-    alert("YOU WIN THE CHAMPIONSHIP! Your winning time was: " + sec + " seconds!!!");
-    window.location.href = "/win.html";
+document.getElementById("finish").onmouseover = function() 
+{
+    if(started)
+    {
+        var sec = secondsSinceEnter();
+        alert("YOU WIN THE CHAMPIONSHIP! Your winning time was: " + sec + " seconds!!!");
+        window.location.href = "/win.html";
+    }
 };
 
 //if user loses
-document.getElementById("hill").onmouseover = function() {
-    var sec = secondsSinceEnter();
-    alert("Oh no! You lost the race! Your final time was: " + sec + " seconds.");
-    window.location.href = "/lose.html";
+document.getElementById("hill").onmouseover = function() 
+{
+    if(started)
+    {
+        var sec = secondsSinceEnter();
+        alert("Oh no! You lost the race! Your final time was: " + sec + " seconds.");
+        window.location.href = "/lose.html";
+    }
 };
 
-document.getElementById("opp").onmouseover = function() {
-    var sec = secondsSinceEnter();
-    alert("Oh no! You lost the race! Your final time was: " + sec + " seconds.");
-  window.location.href = "/lose.html";
+document.getElementById("opp").onmouseover = function() 
+{
+    if(started)
+    {
+        var sec = secondsSinceEnter();
+        alert("Oh no! You lost the race! Your final time was: " + sec + " seconds.");
+        window.location.href = "/lose.html";
+    }
 };
 
-document.getElementById("apple").onmouseover = function() {
-    var sec = secondsSinceEnter();
-    alert("Oh no! You lost the race! Your final time was: " + sec + " seconds.");
-    window.location.href = "/lose.html";
+document.getElementById("apple").onmouseover = function() 
+{
+    if(started)
+    {
+        var sec = secondsSinceEnter();
+        alert("Oh no! You lost the race! Your final time was: " + sec + " seconds.");
+        window.location.href = "/lose.html";
+    }
 };
 
-document.getElementById("peach").onmouseover = function() {
-    var sec = secondsSinceEnter();
-    alert("Oh no! You lost the race! Your final time was: " + sec + " seconds.");
-    window.location.href = "/lose.html";
+document.getElementById("peach").onmouseover = function() 
+{
+    if(started)
+    {
+        var sec = secondsSinceEnter();
+        alert("Oh no! You lost the race! Your final time was: " + sec + " seconds.");
+        window.location.href = "/lose.html";
+    }
 };
 
-document.getElementById("shrub").onmouseover = function() {
-    var sec = secondsSinceEnter();
-    alert("Oh no! You lost the race! Your final time was: " + sec + " seconds.");
-    window.location.href = "/lose.html";
+document.getElementById("shrub").onmouseover = function() 
+{
+    if(started)
+    {
+        var sec = secondsSinceEnter();
+        alert("Oh no! You lost the race! Your final time was: " + sec + " seconds.");
+        window.location.href = "/lose.html";
+    }
 };
 
-document.getElementById("tree").onmouseover = function() {
-    var sec = secondsSinceEnter();
-    alert("Oh no! You lost the race! Your final time was: " + sec + " seconds.");
-    window.location.href = "/lose.html";
+document.getElementById("tree").onmouseover = function() 
+{
+    if(started)
+    {
+        var sec = secondsSinceEnter();
+        alert("Oh no! You lost the race! Your final time was: " + sec + " seconds.");
+        window.location.href = "/lose.html";
+    }
 };
